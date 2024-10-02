0fba511073bc40e204de91267519ce56318ab6bb
anirudh-r5 <anirudh.r515@gmail.com>
2024-10-02 10:47:18 -0700

    added mac config

--- config.el
+++ config.el
@@ -21,7 +21,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 14 :weight 'medium)
+(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 13 :weight 'medium)
       doom-variable-pitch-font (font-spec :family "Alegreya" :size 13))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-horizon)
+(setq doom-theme 'doom-snazzy)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
