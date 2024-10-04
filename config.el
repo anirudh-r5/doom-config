3f00a36c6e37a604946985549d246d572b4a89d4
anirudh-r5 <anirudh.r515@gmail.com>
2024-10-03 19:23:44 -0700

    font spacing

--- config.el
+++ config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
+(setq user-full-name "anirudh-r5"
+      user-mail-address "anirudh.r515@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -21,8 +21,14 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 13 :weight 'medium)
+(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 13 :weight 'semibold)
       doom-variable-pitch-font (font-spec :family "Alegreya" :size 13))
+
+(custom-set-faces!
+  '(font-lock-keyword-face :slant italic))
+
+(setq-default line-spacing 0.20)
+
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
